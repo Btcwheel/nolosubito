3ad104bce74a06f7b218475f1f0d30f7bf4bc53c
Il Tuo Nome <tua@email.com>
2026-05-02 21:26:36 +0200

    Perf: Add cache headers, gzip+brotli compression to reduce TTFB

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import path from 'path'
+import compression from 'vite-plugin-compression'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // Genera file .gz e .br pre-compressi durante il build
+    // Vercel li serve automaticamente al browser che li supporta
+    compression({ algorithm: 'gzip', ext: '.gz' }),
+    compression({ algorithm: 'brotliCompress', ext: '.br' }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
